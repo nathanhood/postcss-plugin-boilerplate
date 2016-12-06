a9499cdd2b3faf5b42fc22c6f2300037dfa9bd07
Nathan Hood <nathanhood@me.com>
2016-12-05 21:01:26 -0600

    require index.js in test file

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const postcss = require('postcss');
-const plugin = require('./');
+const plugin = require('./index.js');
 
 function process(input, expected, opts = {}, warnings = 0) {
 	return postcss([ plugin(opts) ]).process(input)
